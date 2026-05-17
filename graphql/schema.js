d8d1d7ce6773b72a4f28b51c5623170ef98a4ea2
andriy8075 <andriy8075@gmail.com>
2026-05-17 12:41:23 +0100

    bring graphql schema in separate file for readability

--- graphql/schema.js
+++ graphql/schema.js
@@ -1,42 +1,9 @@
+const fs = require('fs');
+const path = require('path');
 const { buildSchema } = require('graphql');
 
-const schema = buildSchema(`
-  type Order {
-    _id: ID!
-    employeeLastName: String!
-    orderAmount: Float!
-    productName: String!
-    clientCompanyName: String!
-    customerLastName: String!
-    createdAt: String
-    updatedAt: String
-  }
-
-  type Query {
-    getOrder(_id: ID!): Order
-    getAllOrders: [Order!]!
-  }
-
-  type Mutation {
-    createOrder(
-      employeeLastName: String!
-      orderAmount: Float!
-      productName: String!
-      clientCompanyName: String!
-      customerLastName: String!
-    ): Order!
-
-    updateOrder(
-      _id: ID!
-      employeeLastName: String!
-      orderAmount: Float!
-      productName: String!
-      clientCompanyName: String!
-      customerLastName: String!
-    ): Order
-
-    deleteOrder(_id: ID!): Boolean!
-  }
-`);
+const schemaPath = path.join(__dirname, 'schema.graphql');
+const schemaSource = fs.readFileSync(schemaPath, 'utf8');
+const schema = buildSchema(schemaSource);
 
 module.exports = schema;
